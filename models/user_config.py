38cbf0d648090832d08016d6adaf78fedd1afdf3
Dipam Chakraborty <dipamc77@gmail.com>
2022-07-29 21:04:10 +0530

    remove baseline files

--- models/user_config.py
+++ models/user_config.py
@@ -1,7 +1,5 @@
 from models.classifiers.random_classifier import RandomClassifier
-# from models.classifiers.bert_classifier import BERTClassifier
 from models.rankers.random_ranker import RandomRanker
 
 UserClassifer = RandomClassifier
-# UserClassifer = BERTClassifier
 UserRanker = RandomRanker
