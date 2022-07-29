a5721468e0c09311d680563c75520d7c7b9b4c45
Dipam Chakraborty <dipam@aicrowd.com>
2022-07-29 13:42:47 +0000

    Update models/user_config.py

--- models/user_config.py
+++ models/user_config.py
@@ -1,4 +1,4 @@
-# from models.classifiers.random_classifier import RandomClassifier
+from models.classifiers.random_classifier import RandomClassifier
 # from models.classifiers.bert_classifier import BERTClassifier
 from models.rankers.random_ranker import RandomRanker
 
